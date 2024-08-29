39ac2da2d73bc7c6bb61f36a706a408c3c666a44
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-29 22:54:44 +0200

    Update productinfo.php

--- api/productinfo.php
+++ api/productinfo.php
@@ -1,12 +1,12 @@
 <?php
 
 /* Ici on va obtenir les informations sur un produit avec toutes les informations disponibles
- * On limit dans le temps à -25jours
+ * On limite au dernier moment
  *
  * */
 
 //init database connection
-$db = new Mysqli('localhost','dbusername','password','dbname');
+$db = new Mysqli('localhost','userdb','passw','dbname');
 
 //connect to the db
 if ($db->connect_error) {
@@ -24,19 +24,23 @@ if(empty($productname)){
     header('Content-Type: application/json');
     die(json_encode(array('error' => 'Productname is required')));
 }
-// on limite à 25 jours
-$sql = "SELECT * FROM `tracking` WHERE `productname` = '$productname' AND `date` > DATE_SUB(NOW(), INTERVAL 25 DAY) ORDER BY `date` DESC";
+// on limite à la derniere entré
+$sql = "SELECT * FROM `tracking` WHERE `productname` = '$productname' ORDER BY `id` DESC LIMIT 1";
+
 
 
 $result = $db->query($sql);
+
 
 if ($result->num_rows > 0) {
     $rows = array();
     while ($row = $result->fetch_assoc()) {
         $rows[] = $row;
     }
+    header('Content-Type: application/json');
     echo json_encode($rows);
 } else {
+    header('Content-Type: application/json');
     echo json_encode(array('error' => 'No data found'));
 }
 
