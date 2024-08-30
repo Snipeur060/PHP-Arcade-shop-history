67b80a2401e354078ad66920148f8d1312591ad1
Snipeur060 <61784407+Snipeur060@users.noreply.github.com>
2024-08-30 11:40:57 +0200

    Update tracker.php
    Petit fix pour plusieurs soucis et bypass des wacome et framework (aucun moyen de les différencier)

--- cron/tracker.php
+++ cron/tracker.php
@@ -4,7 +4,7 @@
  * */
 
 //init database connection
-$db = new Mysqli('localhost','usersnipeurarcadetracker','*X0b549ar','snipeur060db_arcade_tracker');
+$db = new Mysqli('localhost','userdb','pass','dbname');
 
 //connect to the db
 if ($db->connect_error) {
@@ -51,14 +51,27 @@ foreach ($resultarray as $key => $value) {
 
 
 
-    // on ajoute une verification pour savoir s'il y a une difference avec au moins une des valeurs
-    // si oui, on ajoute dans la base de données
-    // sinon, on ne fait rien
-    $sql = "SELECT * FROM `tracking` WHERE `productname` = '$productname' AND `hours` = $hours AND `stock` = $stock AND `imageURL` = '$imageURL' ";
+    // on va faire une verification simple on prend le nom du produit dans la db (la derniere entrée) et on compare avec le nom du produit actuel
+    $sql = "SELECT * FROM `tracking` WHERE `productname` = '$productname' ORDER BY `id` DESC LIMIT 1";
+
+
     $result = $db->query($sql);
-    if ($result->num_rows > 0) {
-        //on passe à la prochaine itération dans le cas où il y a une correspondance exacte
-        continue;
+    if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+        // ça nous permet de remettre les stocks correctement si on fait +1 -1
+        //on fait juste une verif pour Wacome si l'image est https://cloud-c6nos84m4-hack-club-bot.vercel.app/061zj7gvrvsl._ac_sx679_.png on pass
+        if($row['imageURL'] == 'https://cloud-c6nos84m4-hack-club-bot.vercel.app/061zj7gvrvsl._ac_sx679_.png' or $row['imageURL'] == 'https://cloud-bi9lc9poq-hack-club-bot.vercel.app/0intuos.png'){
+            // on ne fait rien
+            continue;
+        }
+        if($row['imageURL']=="https://cloud-g0bjmr0sz-hack-club-bot.vercel.app/013in.png" or $row['imageURL']=="https://cloud-elfpck7gj-hack-club-bot.vercel.app/1screenshot_2024-06-14_at_07.39.22.png" or $row["imageURL"] == 'https://cloud-elfpck7gj-hack-club-bot.vercel.app/1screenshot_2024-06-14_at_07.39.22.png'){
+            // on ne fait rien
+            continue;
+        }
+        if($row['hours'] == $hours && $row['stock'] == $stock){
+            // on ne fait rien
+            continue;
+        }
     }
 
     $sql = "INSERT INTO `tracking`(`productname`, `hours`, `imageURL`,`stock`) VALUES ('$productname',$hours,'$imageURL',$stock)";
